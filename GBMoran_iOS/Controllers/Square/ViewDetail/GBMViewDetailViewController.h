ed0f0dfc58bfa821b835ccb6e509df62f7eaac2e
张和悦 <1084443634@qq.com>
2015-09-26 18:03:00 +0800

    查看图片

---
+++ GBMoran_iOS/Controllers/Square/ViewDetail/GBMViewDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  GBMViewDetailViewController.h
+//  GBMoran_iOS
+//
+//  Created by ZHY on 15/9/26.
+//  Copyright © 2015年 Brave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBMViewDetailViewController : UIViewController
+
+@end
