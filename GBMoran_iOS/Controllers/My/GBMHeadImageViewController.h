cb04f89f4fcbbb92059435dc5cac23bad20a772d
yiqingsheng <250071954@qq.com>
2015-09-26 22:46:51 +0800

    新增我的界面UI

---
+++ GBMoran_iOS/Controllers/My/GBMHeadImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  GBMHeadImageViewController.h
+//  GBMoran_iOS
+//
+//  Created by yikobe_mac on 15/9/26.
+//  Copyright © 2015年 Brave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBMHeadImageViewController : UIViewController
+
+@end
--- GBMoran_iOS/Controllers/My/GBMMyViewController.m
+++ GBMoran_iOS/Controllers/My/GBMMyViewController.m
@@ -10,18 +10,55 @@
 
 @interface GBMMyViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *headImageView;
+
 @end
 
 @implementation GBMMyViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // 给navigation bar 设置背景色，颜色选择和蓦然登录注明界面的button颜色一致
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:237 / 255.0f green:127 / 255.0f blue:74 /255.0f alpha:1.0f];
+    
+    // 把头像显示成圆形
+    self.headImageView.layer.cornerRadius = self.headImageView.frame.size.width / 2.0f;
+    self.headImageView.clipsToBounds = YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
+#pragma mark - TableView Data Source methods
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    CGFloat header;
+    if (section == 0) {
+        header = 13.0;
+    } else if (section == 1) {
+        header = 10;
+    }
+    return header;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0;
+}
+
+#pragma mark - TableView delegate methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 0) {
+        if (indexPath.row == 1) {
+            <#statements#>
+        }
+    }
+}
 
 
 
