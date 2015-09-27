f45875e2b1b903bd71da775bf7e39a5059d21fae
张和悦 <1084443634@qq.com>
2015-09-27 13:23:34 +0800

    更新查看图片

---
+++ GBMoran_iOS/Controllers/Square/ViewDetail/GBMViewDetailViewController.h
@@ -0,0 +1,19 @@
+//
+//  GBMViewDetailViewController.h
+//  GBMoran_iOS
+//
+//  Created by ZHY on 15/9/26.
+//  Copyright © 2015年 Brave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBMViewDetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *ViewDetailImage;
+@property (weak, nonatomic) IBOutlet UILabel *TitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *AddressLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *UserImage;
+@property (weak, nonatomic) IBOutlet UILabel *UserNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *TimeLabel;
+
+@end
